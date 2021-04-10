a6757ac3ad67b9a030b82868b90938dbb6429b9e
Sc0tticus <ssinger303@gmail.com>
2021-04-09 21:21:00 -0600

    Added Yelp Icon to page. Success thru #04

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import yelpLogo from '../assets/yelpLogo.png';
+import styles from './LandingPage.module.css';
 
 export function LandingPage() {
 	return (
 		<div>
 			<TopNav />
+			<img src={yelpLogo} className={styles.yelpLogo} alt="logo" />
 		</div>
 	);
 }
