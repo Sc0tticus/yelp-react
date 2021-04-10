ff6a17be6bbc578d01061deef08863b279e86076
Sc0tticus <ssinger303@gmail.com>
2021-04-09 19:55:46 -0600

    success thru YT #02

---
+++ src/LandingPage/TopNav/TopNav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function TopNav() {
+	return (
+		<div>
+			<div>
+				<span>Write a Review</span>
+				<span>Events</span>
+			</div>
+			<div>
+				<span>Login</span>
+				<button className="button">Sign up</button>
+			</div>
+		</div>
+	);
+}
