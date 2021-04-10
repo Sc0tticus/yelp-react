44dc87f4de99db8784266b15feaf49c9d7a5bfd2
Sc0tticus <ssinger303@gmail.com>
2021-04-09 22:17:19 -0600

    Updated SearchBar.js and styled Search Bar. 6S thru #05

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { TopNav } from './TopNav/TopNav';
 import yelpLogo from '../assets/yelpLogo.png';
 import styles from './LandingPage.module.css';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 export function LandingPage() {
 	return (
 		<div>
 			<TopNav />
 			<img src={yelpLogo} className={styles.yelpLogo} alt="logo" />
+			<SearchBar />
 		</div>
 	);
 }
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+export function SearchBar() {
+	return (
+		<div className="field has-addons">
+			<p className="control">
+				<button className="button is-static is-medium">Search</button>
+			</p>
+			<p className="control">
+				<input
+					className={`input is-medium ${styles['input-control']}`}
+					type="text"
+					placeholder="burgers, barbers, parking spots"
+				/>
+			</p>
+			<p className="control">
+				<button className="button is-static is-medium">Near</button>
+			</p>
+			<p className="control">
+				<input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Where"></input>
+			</p>
+		</div>
+	);
+}
