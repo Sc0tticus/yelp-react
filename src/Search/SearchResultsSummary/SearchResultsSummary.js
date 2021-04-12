be9f8157f01820612aa074dcca0ed507e6b42a77
Sc0tticus <ssinger303@gmail.com>
2021-04-12 13:02:17 -0600

    Added Filter Icons and $ symbols on the Icons in the Search Results gray section below the search bar

--- src/Search/SearchResultsSummary/SearchResultsSummary.js
+++ src/Search/SearchResultsSummary/SearchResultsSummary.js
@@ -10,6 +10,32 @@ export function SearchResultsSummary() {
 				</h1>
 				<p>Showing 1-20 results out of 534 results</p>
 			</div>
+			<div className={styles.filters}>
+				<button className="button">
+					<span className="icon">
+						<i className="fas fa-sliders-h"></i>
+					</span>
+					<span>All Filters</span>
+				</button>
+				<div className="buttons has-addons">
+					<button className="button">$</button>
+					<button className="button">$$</button>
+					<button className="button">$$$</button>
+					<button className="button">$$$$</button>
+				</div>
+				<button className="button">
+					<span className="icon">
+						<i className="fas fa-clock"></i>
+					</span>
+					<span>Open Now</span>
+				</button>
+				<button className="button">
+					<span className="icon">
+						<i className="fas fa-dollar-sign"></i>
+					</span>
+					<span>Cash Back</span>
+				</button>
+			</div>
 		</div>
 	);
 }
