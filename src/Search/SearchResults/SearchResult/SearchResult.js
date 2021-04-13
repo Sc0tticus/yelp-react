37c26a372c483936ec990c16644b9b6a03fe43fe
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:08:26 -0600

    updated SearchResult.js to match prod coder

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import { BusinessRating } from '../../../BusinessRating/BusinessRating';
 import styles from './SearchResult.module.css';
+import { BusinessRating } from '../../../BusinessRating/BusinessRating';
 
 export function SearchResult(props) {
-	const biz = props.business;
-
-	if (!props.business) {
-		return <div></div>;
+	const b = props.business;
+	if (!b) {
+		return <div />;
 	}
 
-	const tags = biz.categories.map(category => (
-		<span className={`tag ${styles['business-tag']}`} key={biz.id + category.title}>
+	const tags = b.categories.map(category => (
+		<span className={`tag ${styles['business-tag']}`} key={b.id + category.title}>
 			{category.title}
 		</span>
 	));
-	const adressLines = biz.location.display_address.map(adressLine => <p key={biz.id + adressLine}>{adressLine}</p>);
+	const addressLines = b.location.display_address.map(addressLine => <p key={b.id + addressLine}>{addressLine}</p>);
 
 	return (
-		<div>
-			SearchResult
-			<div className={styles['search-result']}>
-				<img src={biz.image_url} alt="business" className={styles['business-image']} />
-				<div className={styles['business-info']}>
-					<h2 className="subtitle">{biz.name}</h2>
-					<BusinessRating reviewCount={biz.review_count} rating={biz.rating} />
-					<p>
-						{biz.price} {tags}
-					</p>
-				</div>
-				<div className={styles['contact-info']}>
-					<p>{biz.phone}</p>
-					{adressLines}
-				</div>
+		<div className={styles['search-result']}>
+			<img src={b.image_url} alt="business" className={styles['business-image']} />
+			<div className={styles['business-info']}>
+				<h2 className="subtitle">{b.name}</h2>
+				<BusinessRating reviewCount={b.review_count} rating={b.rating} />
+				<p>
+					{b.price} {tags}
+				</p>
+			</div>
+			<div className={styles['contact-info']}>
+				<p>{b.phone}</p>
+				{addressLines}
 			</div>
 		</div>
 	);
