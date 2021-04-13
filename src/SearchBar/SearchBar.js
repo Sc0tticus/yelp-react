226b3a8820733b25d44c79a23520036543b909cb
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:12:41 -0600

    Updated SearchBar.js to match prod coder

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,49 +2,50 @@ import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 export function SearchBar(props) {
-	// please introduce some state into our functional component.
 	const [term, setTerm] = useState(props.term || '');
-	const [location, setLocation] = useState(props.term || '');
+	const [location, setLocation] = useState(props.location || '');
 
-	const sizeClass = props.small ? '' : 'is-medium';
-
-	function onSubmit(e) {
+	function submit(e) {
 		if (typeof props.search === 'function') {
 			props.search(term, location);
 		}
+		console.log(term, location);
 		e.preventDefault();
 	}
 
+	const sizeClass = props.small ? '' : 'is-medium';
 	return (
-		<form onSubmit={onSubmit}>
-			<p className="control">
-				<button className={`button is-static ${sizeClass}`}>Search</button>
-			</p>
-			<p className="control">
-				<input
-					onChange={e => setTerm(e.target.value)}
-					className={`input ${sizeClass} ${styles['input-control']}`}
-					type="text"
-					value={term}
-					placeholder="burgers, barbers, parking spots"
-				/>
-			</p>
-			<div className="control">
-				<button className={`button is-static ${sizeClass}`}>Near</button>
-			</div>
-			<p className="control">
-				<input
-					className={`input ${sizeClass} ${styles['input-control']}`}
-					onChange={e => setLocation(e.target.value)}
-					type="text"
-					value={location}
-					placeholder="Where"
-				></input>
-			</p>
-			<div className={`button ${sizeClass} ${styles['search-button']}`} onClick={onSubmit}>
-				<span className={`icon is-small ${styles['search-icon']}`}>
-					<i className={`fas fa-search ${styles['search-icon']}`}></i>
-				</span>
+		<form onSubmit={submit}>
+			<div className="field has-addons">
+				<p className="control">
+					<button className={`button is-static ${sizeClass}`}>Search</button>
+				</p>
+				<p className="control">
+					<input
+						className={`input ${sizeClass} ${styles['input-control']}`}
+						onChange={e => setTerm(e.target.value)}
+						type="text"
+						value={term}
+						placeholder="burgers, barbers, spas, handymen"
+					/>
+				</p>
+				<div className="control">
+					<div className={`button is-static ${sizeClass}`}>NEAR</div>
+				</div>
+				<p className="control">
+					<input
+						className={`input ${sizeClass} ${styles['input-control']}`}
+						onChange={e => setLocation(e.target.value)}
+						type="text"
+						value={location}
+						placeholder="Where"
+					/>
+				</p>
+				<div className={`button ${sizeClass} ${styles['search-button']}`} onClick={submit}>
+					<span className={`icon is-small ${styles['search-icon']}`}>
+						<i className="fas fa-search"></i>
+					</span>
+				</div>
 			</div>
 		</form>
 	);
