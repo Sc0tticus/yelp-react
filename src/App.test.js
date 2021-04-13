6a9b782cf931b3516a0f07ac2e203789aecf79fc
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:17:52 -0600

    Updated App.test.js to match prod coder, but commented out code, to test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,23 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { MemoryRouter } from 'react-router-dom';
+
+// it('renders without crashing', () => {
+// 	const div = document.createElement('div');
+// 	ReactDOM.render(
+// 		<MemoryRouter>
+// 			<App />
+// 		</MemoryRouter>,
+// 		div
+// 	);
+// 	ReactDOM.unmountComponentAtNode(div);
+// });
