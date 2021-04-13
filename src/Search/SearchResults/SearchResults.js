9af1e3ab85d6379922fbb32f4e7a30af43fdcf71
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:09:32 -0600

    updated SearchResults.js to match prod coder

--- src/Search/SearchResults/SearchResults.js
+++ src/Search/SearchResults/SearchResults.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { SearchResult } from './SearchResult/SearchResult';
 import styles from './SearchResults.module.css';
-import { Spinner } from '../../Spinner/Spinner';
+import { Spinner } from '../../Spinner/Spinner.js';
 
 export function SearchResults(props) {
 	let searchResults = <Spinner />;
-
 	if (props.businesses && props.businesses.length) {
-		searchResults = props.businesses.map(biz => <SearchResult key={biz.id} business={biz} />);
+		searchResults = props.businesses.map(b => <SearchResult key={b.id} business={b} />);
 	}
 
 	return <div className={styles['search-results']}>{searchResults}</div>;
