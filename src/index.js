5bad076004dae5c981eb3f3c371488d319ee7198
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:18:50 -0600

    Updated index.js to match prod coder. Commented out his code to test

--- src/index.js
+++ src/index.js
@@ -19,3 +19,22 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import 'bulma/css/bulma.css';
+// import * as serviceWorker from './serviceWorker';
+// import { BrowserRouter } from 'react-router-dom';
+
+// ReactDOM.render(
+// 	<BrowserRouter>
+// 		<App />
+// 	</BrowserRouter>,
+// 	document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
