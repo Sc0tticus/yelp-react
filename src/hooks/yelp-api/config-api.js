b630f9db136298af4e288b3f55c887c51d3ef94f
Sc0tticus <ssinger303@gmail.com>
2021-04-13 08:35:25 -0600

    Success thru CH 25

--- src/hooks/yelp-api/config-api.js
+++ src/hooks/yelp-api/config-api.js
@@ -5,5 +5,6 @@ export const API_BASE_URL = 'https://api.yelp.com/v3';
 // NOTE: as long as this token is saved in the frontend, the application is not production ready.
 // The Bearer token must be used on a server whose inner workings are not exposed to the outside world.
 // For sake of time keeping bearer token in frontend code. Would take out this out for production.
+
 export const BEARER_TOKEN =
 	'eNaf0vZMUNd-TRwWzw1G4sokf22oJfhdyVrV7EJ_a9jTGXFXZ6BEd-sSw7E0EI00pF5hd3Qa0wnU6vTnJWA2EF5VcV2zhGnEotJtGB7hA_Q8MRk-BlePjxhwL0FpYHYx';
