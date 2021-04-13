4643391743c5dcb5a9ec1afb55c46f8da1ea8782
Sc0tticus <ssinger303@gmail.com>
2021-04-13 14:58:04 -0600

    Updated SearchSuggestion.js to prod coder version

--- src/LandingPage/SearchSuggestions/SearchSuggestions.js
+++ src/LandingPage/SearchSuggestions/SearchSuggestions.js
@@ -5,19 +5,19 @@ export function SearchSuggestions() {
 	return (
 		<div className={styles.suggestions}>
 			<span className="icon is-small">
-				<i className={`fas fa-utensils ${styles['search-icon']}`}></i>
+				<i className="fas fa-utensils"></i>
 			</span>
 			<span className={styles.suggestion}>Restaurants</span>
 			<span className="icon is-small">
-				<i className={`fas fa-cocktail ${styles['search-icon']}`}></i>
+				<i className="fas fa-cocktail"></i>
 			</span>
 			<span className={styles.suggestion}>Nightlife</span>
 			<span className="icon is-small">
-				<i className={`fas fa-concierge-bell ${styles['search-icon']}`}></i>
+				<i className="fas fa-concierge-bell"></i>
 			</span>
 			<span className={styles.suggestion}>Services</span>
 			<span className="icon is-small">
-				<i className={`fas fa-truck ${styles['search-icon']}`}></i>
+				<i className="fas fa-truck"></i>
 			</span>
 			<span className={styles.suggestion}>Delivery Services</span>
 		</div>
