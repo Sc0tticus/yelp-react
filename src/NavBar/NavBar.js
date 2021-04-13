024302e55bc821250cc5525e34c8c043fd6c91e1
Sc0tticus <ssinger303@gmail.com>
2021-04-13 15:07:30 -0600

    Updated NavBar.js and CSS to match prod coder

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,5 +1,24 @@
+// import React from 'react';
+import yelpLogo from '../assets/yelpLogo.png';
+// import { SearchBar } from '../SearchBar/SearchBar';
+// import styles from './NavBar.module.css';
+// import { Link } from 'react-router-dom';
+
+// export function NavBar(props) {
+// 	return (
+// 		<div className={styles['nav-bar']}>
+// 			<Link to="/">
+// 				<img src={yelpLogo} className={styles['nav-bar']} alt="yelp logo" />
+// 			</Link>
+// 			<SearchBar small term={props.term} location={props.location} search={props.search} />
+// 			<button className={`button ${styles['nav-button']}`}>Sign In</button>
+// 			<button className={`button ${styles['nav-button']}`}>Register</button>
+// 		</div>
+// 	);
+// }
+
 import React from 'react';
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
 import styles from './NavBar.module.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { Link } from 'react-router-dom';
@@ -8,7 +27,7 @@ export function NavBar(props) {
 	return (
 		<div className={styles['nav-bar']}>
 			<Link to="/">
-				<img src={logo} className={styles.logo} alt="belb logo" />
+				<img src={yelpLogo} className={styles.logo} alt="belb logo" />
 			</Link>
 			<SearchBar small term={props.term} location={props.location} search={props.search} />
 			<button className={`button ${styles['nav-button']}`}>Sign In</button>
